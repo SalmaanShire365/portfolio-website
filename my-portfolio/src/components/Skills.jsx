f85dffe3720110a51fb69b262e875a143883f4f5
SalmaanShire365 <salmaanshire@gmail.com>
2026-03-09 00:28:57 -0500

    Downgrade react-refresh rule to warn

--- my-portfolio/src/components/Skills.jsx
+++ my-portfolio/src/components/Skills.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-function Skills() {
+export const Skills = () =>  {
     return (
         <><h2>Technical Skills</h2><p>Java, Python, MySQL</p><p>Spring Boot,React, Node, Flask</p></>
     )};
+export default Skills;
